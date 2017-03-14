72d4b99751f2661742b7618610ae40f999ee17e5
nevernervous <brndncostoya@gmail.com>
2017-03-14 15:58:35 -0700

    mysql users
    Former-commit-id: 1240407debc526ceeecd3d1927697eb9599b6988

--- config/mysql-setup.sql
+++ config/mysql-setup.sql
@@ -1,5 +1,6 @@
 grant all privileges on *.* to 'root'@'localhost' identified by '';
 CREATE DATABASE the_peak_beyond ;
 grant all on the_peak_beyond to 'tpb'@'localhost' identified by 'tpb2017';
+FLUSH PRIVILEGES;
 
 
