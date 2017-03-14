61699b23ecd3c0184de11fbac5913ce0d0fc4e4f
nevernervous <brndncostoya@gmail.com>
2017-03-14 15:07:17 -0700

    fix typo on tpb.conf; run mysql commands from sql file (prettier)
    Former-commit-id: a10cb4dc2dd22a5146a34dccd3ac3c4d5539866c

---
+++ config/mysql-setup.sql
@@ -0,0 +1,4 @@
+--todo: secure settings for the MySQL DB should be determined and set up here
+grant all privileges on *.* to 'root'@'localhost' identified by '';
+CREATE DATABASE the_peak_beyond ;
+
