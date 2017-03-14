8e35f6d39fc124564df303da6a3dd292c19c19f0
nevernervous <brndncostoya@gmail.com>
2017-03-14 15:39:14 -0700

    path to apache config
    Former-commit-id: cabde09f460437e949fe93754d204eb09cba9e7c

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ sudo apt-get install -y -q apache2
 
 # copy apache config into place
 sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.old
-sudo cp ./apache/apache2.conf /etc/apache2/apache2.conf
+sudo cp ./config/apache/apache2.conf /etc/apache2/apache2.conf
 sudo systemctl restart apache2
 
 # enable rewrite for wordpress
