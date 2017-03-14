4bf243f12f62931bc0f81e2e8f47aebd9d4953f3
nevernervous <brndncostoya@gmail.com>
2017-03-14 15:25:51 -0700

    remove sql comment; move files to tmp dir
    Former-commit-id: 70a5d7fdc480497fd9c932f76997ed715b690a76

--- config/mysql-setup.sql
+++ config/mysql-setup.sql
@@ -1,4 +1,3 @@
---todo: secure settings for the MySQL DB should be determined and set up here
 grant all privileges on *.* to 'root'@'localhost' identified by '';
 CREATE DATABASE the_peak_beyond ;
 
