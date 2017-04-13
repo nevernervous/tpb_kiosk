2ae347ce0f31f84061c1d0bb62d14c04738ac160
nevernervous <brndncostoya@gmail.com>
2017-04-13 15:34:46 +0200

    option to disable object recognition from backoffice
    Former-commit-id: 4e306bf1fe7b6028cd15a68154b5695bdf498e28

--- latestbuild/www/wp-content/themes/tpb/index.php
+++ latestbuild/www/wp-content/themes/tpb/index.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 
 	<div class="site-main is-hidden">
-		<?php get_template_part( 'screen-select' ); ?>
+		<?php
+			if ( false === get_field( 'object_recognition', 'options' ) )
+				get_template_part( 'screen-catalogue' );
+			else
+				get_template_part( 'screen-select' );
+		?>
 	</div><!-- .site-main -->
 
 <?php get_footer(); ?>
