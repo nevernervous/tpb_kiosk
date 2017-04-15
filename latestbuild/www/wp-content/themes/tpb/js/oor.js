25884a81fe9a25ccc9b6e1c173c154652f2db4e7
root <root@ip-172-31-15-250.us-west-2.compute.internal>
2017-04-15 18:18:27 +0000

    add files
    Former-commit-id: 6e95ac8a663af9f51a20d9b243b19ba18e0fbbec

--- latestbuild/www/wp-content/themes/tpb/js/oor.js
+++ latestbuild/www/wp-content/themes/tpb/js/oor.js
@@ -1,5 +1,5 @@
 A_DISTANCE = 17.1;
-MARGIN = 4.7;
+MARGIN = 6;
 
 // get the (flawed) screenPixel To MillimeterX value.
 var calibration = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
